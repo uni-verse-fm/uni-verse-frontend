9774eccd4c9957138fc78632dd6fd8b2b87fbaf9
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-06-04 19:23:07 +1000

    logout: implement

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { goto } from '$app/navigation';
 	import Icon from '$lib/images/uni-verse-logo.svg';
+	import { getContext } from 'svelte';
 
 	function handleLogin() {
 		goto('/auth/login');
@@ -9,6 +10,8 @@
 	function handleRegister() {
 		goto('/auth/register');
 	}
+
+	let session = getContext('session');
 </script>
 
 <header>
@@ -20,8 +23,15 @@
 		SearchBar here<!-- <SearchBar  /> -->
 	</div>
 	<div class="authSection">
-		<button class="loginButton" on:click={handleLogin}>Login</button>
-		<button class="registerButton" on:click={handleRegister}>Register</button>
+		{#if $session}
+			<span> {$session.username} </span>
+			<form method="POST" action="/auth/logout">
+				<button class="logoutButton">Logout</button>
+			</form>
+		{:else}
+			<button class="loginButton" on:click={handleLogin}>Login</button>
+			<button class="registerButton" on:click={handleRegister}>Register</button>
+		{/if}
 	</div>
 </header>
 
@@ -43,6 +53,15 @@
 	header * {
 		margin-top: auto;
 		margin-bottom: auto;
+	}
+
+	.authSection {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.authSection * {
+		margin: auto 1em;
 	}
 
 	.branding {
