95f2658c8c52e15ddda6c6182b573b546145d9b9
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-06-04 17:16:16 +1000

    login-form: interface

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+	import GenericFormField from '$lib/components/generic-form-field.svelte';
+	import { selectClassValidatorErrors } from '$lib/helpers';
+
+	export let form;
+</script>
+
+<div class="register-card">
+	<form method="POST">
+		<GenericFormField
+			field="email"
+			value={form?.email?.toString()}
+			errors={selectClassValidatorErrors('email', form?.error)}
+			type="email"
+			required
+		/>
+		<GenericFormField
+			field="password"
+			errors={selectClassValidatorErrors('password', form?.error)}
+			type="password"
+			required
+		/>
+
+		<button>Login</button>
+	</form>
+</div>
+
+<style>
+	.register-card {
+		background-color: var(--overlay);
+		border-radius: var(--rounding);
+		padding: 3em;
+		width: 30em;
+		margin: auto;
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+		margin: auto;
+	}
+
+	button {
+		margin-top: 1em;
+	}
+</style>
