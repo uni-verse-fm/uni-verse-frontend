a166a6eb0c9bc56bf97f4a6759c0e194b0c87f46
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-06-07 10:51:26 +1000

    env checks: totally disable

--- src/lib/api/conf.ts
+++ src/lib/api/conf.ts
@@ -1,8 +1,8 @@
 import { env } from '$env/dynamic/public';
 
-if (process.env.NODE_ENV == "production" && !env.PUBLIC_SERVER_SIDE_API_BASE_URL) {
-  throw new Error('PUBLIC_SERVER_SIDE_API_BASE_URL MUST BE SET!');
-}
+//if (process.env.NODE_ENV == "production" && !env.PUBLIC_SERVER_SIDE_API_BASE_URL) {
+//  throw new Error('PUBLIC_SERVER_SIDE_API_BASE_URL MUST BE SET!');
+//}
 
 export default {
   apiBaseUrl: env.PUBLIC_SERVER_SIDE_API_BASE_URL
