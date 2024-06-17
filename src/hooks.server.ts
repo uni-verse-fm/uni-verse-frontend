4406345b021187961a9919053d5262beb0ed66f5
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-06-17 20:45:58 +1000

    hooks: handle session

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,70 @@
-import type { HandleFetch } from '@sveltejs/kit';
+import { error, type Handle, type HandleFetch } from '@sveltejs/kit';
 import apiConf from '$lib/api/conf';
+import { buildSessionCookie, isTokenExpired, parseSessionCookie } from '$lib/session';
 
-export const handleFetch: HandleFetch = ({ request, event, fetch }) => {
-  if (request.url.startsWith(apiConf.apiBaseUrl)) {
-    // Check the current token and refresh if needed
-    const session = JSON.parse(event.cookies.get('session') ?? '{}');
+const handleApifetch: HandleFetch = async ({ request, event, fetch }) => {
+  const sessionCookie = event.cookies.get('session');
 
-    console.log(session);
+  if (!sessionCookie) {
+    return fetch(request);
   }
 
+  const parsedSessionCookie = parseSessionCookie(sessionCookie);
+
+  let accessToken = parsedSessionCookie.accessToken;
+
+  if (isTokenExpired(parsedSessionCookie.accessToken)) {
+    const res = await fetch(`${apiConf.apiBaseUrl}/auth/refresh`, {
+      headers: {
+        Authorization: `Bearer ${parsedSessionCookie.refreshToken}`
+      }
+    });
+
+    if (!res.ok) {
+      event.cookies.delete('session', { path: '/' });
+      return error(401, { message: 'Could not refresh your session token, please login again' });
+    }
+
+    const payload = await res.json();
+
+    event.cookies.set(
+      'session',
+      buildSessionCookie({
+        ...parsedSessionCookie,
+        accessToken: payload.accessToken
+      }),
+      { path: '/' }
+    );
+
+    accessToken = payload.accessToken;
+  }
+
+  request.headers.set('Authorization', `Bearer ${accessToken}`);
+
   return fetch(request);
+};
+
+export const handleFetch: HandleFetch = async (input) => {
+  if (input.request.url.startsWith(apiConf.apiBaseUrl)) {
+    return handleApifetch(input);
+  }
+
+  return fetch(input.request);
+};
+
+export const handle: Handle = async ({ resolve, event }) => {
+  const sessionCookie = event.cookies.get('session');
+
+  if (!sessionCookie) {
+    return resolve(event);
+  }
+
+  const parsedSessionCookie = parseSessionCookie(sessionCookie);
+
+  if (isTokenExpired(parsedSessionCookie.refreshToken)) {
+    event.cookies.delete('session', { path: '/' });
+    return error(401, { message: 'Session expired. Please login again' });
+  }
+
+  return resolve(event);
 };
