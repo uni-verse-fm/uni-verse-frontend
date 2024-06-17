057b64e73012d45fb45c518ef01ed85c388ec16b
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-06-17 20:48:28 +1000

    server: adapt to new session

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,13 @@
 import type { ServerLoadEvent } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
+import { parseSessionCookie } from "$lib/session";
 
 export const load: LayoutServerLoad = ({ cookies }: ServerLoadEvent) => {
   const session = cookies.get("session");
 
   if (session)
     try {
-      return { session: JSON.parse(session) }
+      return { session: parseSessionCookie(session) }
     } catch {
       return {}
     }
