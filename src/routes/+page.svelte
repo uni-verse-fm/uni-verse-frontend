949cdac8b580e5905381403b6214cf7e9903c3d2
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-06-19 11:45:25 +1000

    homepage: link to releases

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+	import { goto } from "$app/navigation";
+
 </script>
 
 <h1>Welcome to Uni-verse!</h1>
+<button on:click={() => goto("/releases")}>Releases</button>
+
 <p>
 	Visit <a href="https://github.com/uni-verse-fm">our github</a> to read more about this project!
 </p>
